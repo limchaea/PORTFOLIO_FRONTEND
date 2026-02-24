4a6cd884d3fdc99f53b9b335089f1cfdc0069fd6
Lim Chae A <imchae4@gmail.com>
2026-02-25 01:15:51 +0900

    init : project page tsx, scss

---
+++ app/projects/page.tsx
@@ -0,0 +1,9 @@
+import './scss/projects.scss';
+
+export default function ProjectPage() {
+  return (
+    <section id="Project" className="projectsPage wideInner">
+      <div className="inner"></div>
+    </section>
+  );
+}
