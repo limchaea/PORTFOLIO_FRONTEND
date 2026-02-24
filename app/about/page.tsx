ceb502ab22173bd2c265a9e63f068bd927f59764
Lim Chae A <imchae4@gmail.com>
2026-02-25 01:04:46 +0900

    init : about page tsx, scss

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import './scss/about.scss';
+
+export default function AboutPage() {
+  return (
+    <section id="About" className="wideInner">
+      <div className="inner"></div>
+    </section>
+  );
+}
