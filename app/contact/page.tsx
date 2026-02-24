1b0ddff2742bc43ba59e38d1dabb7d697159b58b
Lim Chae A <imchae4@gmail.com>
2026-02-25 01:15:08 +0900

    init : contact page tsx, scss

---
+++ app/contact/page.tsx
@@ -0,0 +1,9 @@
+import './scss/contact.scss';
+
+export default function ContactPage() {
+  return (
+    <section id="Contact" className="contactPage wideInner">
+      <div className="inner"></div>
+    </section>
+  );
+}
